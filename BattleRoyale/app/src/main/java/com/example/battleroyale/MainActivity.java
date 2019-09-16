9def1b649576c47c513293c8af965247f8b27906
Sam Adeniyi <samadeniyi81@gmail.com>
2019-09-16 10:16:10 -0500

    Added Joystick to main activity

--- BattleRoyale/app/src/main/java/com/example/battleroyale/MainActivity.java
+++ BattleRoyale/app/src/main/java/com/example/battleroyale/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.battleroyale;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,37 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        final TextView mTextViewAngleLeft = (TextView) findViewById(R.id.textView_angle_left);
-//        final TextView mTextViewStrengthLeft = (TextView) findViewById(R.id.textView_strength_left);
-//
-//        JoystickView joystickLeft = (JoystickView) findViewById(R.id.joystickView_left);
-//        joystickLeft.setOnMoveListener(new JoystickView.OnMoveListener() {
-//            @Override
-//            public void onMove(int angle, int strength) {
-//                mTextViewAngleLeft.setText(angle + "°");
-//                mTextViewStrengthLeft.setText(strength + "%");
-//            }
-//        });
-//
-//        final TextView mTextViewAngleRight = (TextView) findViewById(R.id.textView_angle_right);
-//        final TextView mTextViewStrengthRight = (TextView) findViewById(R.id.textView_strength_right);
-//        final TextView mTextViewCoordinateRight = findViewById(R.id.textView_coordinate_right);
-//
-//        final JoystickView joystickRight = (JoystickView) findViewById(R.id.joystickView_right);
-//        joystickRight.setOnMoveListener(new JoystickView.OnMoveListener() {
-//            @SuppressLint("DefaultLocale")
-//            @Override
-//            public void onMove(int angle, int strength) {
-//                mTextViewAngleRight.setText(angle + "°");
-//                mTextViewStrengthRight.setText(strength + "%");
-//                mTextViewCoordinateRight.setText(
-//                        String.format("x%03d:y%03d",
-//                                joystickRight.getNormalizedX(),
-//                                joystickRight.getNormalizedY())
-//                );
-//            }
-//        });
 
     }
 }
