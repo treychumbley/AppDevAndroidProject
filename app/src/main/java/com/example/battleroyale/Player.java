5dcb7fbfba9edab8f5494bc0d5805cbd0a5c27a0
treychumbley <trey.chumbley@my.simpson.edu>
2019-09-27 10:19:04 -0500

    Update Player Class

--- app/src/main/java/com/example/battleroyale/Player.java
+++ app/src/main/java/com/example/battleroyale/Player.java
@@ -3,5 +3,9 @@ package com.example.battleroyale;
 //creates players
 //keeps track of players health/location/ammo/current weapon
 
-public class PlayerClass {
+public class Player {
+    int health = 100;
+    int xLocation;
+    int yLocation;
+    int ammo;
 }
