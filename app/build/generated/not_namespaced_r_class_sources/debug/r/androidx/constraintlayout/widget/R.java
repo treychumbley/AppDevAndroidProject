9b31f3bc7d6c3199a1f6bfb2e3284309218c85a3
dcxoreo7 <dc.scott@my.simpson.edu>
2019-10-21 21:29:58 -0500

    Created instance where user can save/load a username to a text file

--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f070023;
-        public static final int end = 0x7f070039;
-        public static final int gone = 0x7f070041;
-        public static final int invisible = 0x7f07004d;
-        public static final int left = 0x7f070051;
-        public static final int packed = 0x7f07005f;
-        public static final int parent = 0x7f070060;
-        public static final int percent = 0x7f070062;
-        public static final int right = 0x7f070068;
-        public static final int spread = 0x7f070081;
-        public static final int spread_inside = 0x7f070082;
-        public static final int start = 0x7f070087;
-        public static final int top = 0x7f070098;
-        public static final int wrap = 0x7f0700a2;
+        public static final int end = 0x7f07003c;
+        public static final int gone = 0x7f070044;
+        public static final int invisible = 0x7f070050;
+        public static final int left = 0x7f070054;
+        public static final int packed = 0x7f070062;
+        public static final int parent = 0x7f070063;
+        public static final int percent = 0x7f070065;
+        public static final int right = 0x7f07006b;
+        public static final int spread = 0x7f070084;
+        public static final int spread_inside = 0x7f070085;
+        public static final int start = 0x7f07008a;
+        public static final int top = 0x7f07009b;
+        public static final int wrap = 0x7f0700a5;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/example/battleroyale/ProfileActivity.java
+++ app/src/main/java/com/example/battleroyale/ProfileActivity.java
@@ -4,9 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ProfileActivity extends AppCompatActivity {
+    private static final String FILE_NAME = "example.txt";
+
+    EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +27,64 @@ public class ProfileActivity extends AppCompatActivity {
 
         //Name display
         TextView userProfileName = findViewById(R.id.userProfileName);
+
+        mEditText = findViewById(R.id.edit_text);
+    }
+
+    public void save(View v) {
+        String text = mEditText.getText().toString();
+        FileOutputStream fos = null;
+
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+
+            //mEditText.getText().clear();
+            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME, Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void load(View v) {
+        FileInputStream fis = null;
+
+        try {
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+
+            while ((text = br.readLine()) != null){
+                sb.append(text).append("\n");
+            }
+
+            mEditText.setText(sb.toString());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 }
